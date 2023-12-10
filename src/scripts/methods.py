88612efe426145ae6df4f8c02e8648805dc9a0be
Katherine Rosenfeld <krosenfeld@idmod.org>
2023-12-10 22:28:54 +0000

    starting calibration figure

--- src/scripts/methods.py
+++ src/scripts/methods.py
@@ -10,6 +10,56 @@ class DefaultColors(BaseModel):
     R0: str = "k"
     emod: str = "C0"
 
+class CalibBrick(BaseModel):
+    param_calib: dict
+    param_dict: dict
+    data_brick: dict
+    data: dict = {}
+
+    def __init__(self, tpath=None, **data):
+            
+        if tpath is not None:
+            # import simulation results
+            with open(tpath / 'param_dict.json') as fid01:
+                param_calib = json.load(fid01)
+            with open(tpath / 'param_dict_iters.json') as fid01:
+                param_dict = json.load(fid01)
+            with open(tpath / 'data_brick_iters.json') as fid01:
+                data_brick = json.load(fid01)
+
+            super().__init__(param_calib=param_calib, param_dict=param_dict, data_brick=data_brick)
+        else:
+            super().__init__(**data)
+
+        self._parse_brick()
+
+    def _parse_brick(self):
+        # number of calibration iterations
+        num_iter = self.param_calib["NUM_ITER"]
+        
+        data = pd.DataFrame()
+        for it in range(num_iter):
+            # format of iteration number stored in the dictionaries
+            # https://github.com/InstituteforDiseaseModeling/EMOD-Generic-Scripts/blob/main/model_demographics01/experiment_demog_UK01_calib/make06_pool_brick.py
+            fmt = "{:02d}"
+            # construct data brick
+            db = DataBrick(data_brick=self.data_brick[fmt.format(it)], param_dict=self.param_dict[fmt.format(it)])            
+            db_df = db.to_df()
+            # add iteration 
+            db_df["iteration"] = it
+            # concatenate
+            data = pd.concat([data, db_df], ignore_index=True)
+
+        # reset index
+        data = data.reset_index(drop=True)
+
+        self.data = data.to_dict()
+
+    def to_df(self) -> pd.DataFrame:
+        # the data is a dict of dicts, so we 
+        return pd.DataFrame(self.data)
+
+
 class DataBrick(BaseModel):
     """
     Class to turn EMOD results into a pandas dataframe
@@ -20,15 +70,18 @@ class DataBrick(BaseModel):
     data: dict = {}
     varables: list = []
 
-    def __init__(self, tpath):
+    def __init__(self, tpath=None, **data):
 
-        # import simulation results
-        with open(tpath / 'data_brick.json') as fid01:
-            data_brick = json.load(fid01)
-        with open(tpath / 'param_dict.json') as fid01:
-            param_dict = json.load(fid01)
+        if tpath is not None:
+            # import simulation results
+            with open(tpath / 'data_brick.json') as fid01:
+                data_brick = json.load(fid01)
+            with open(tpath / 'param_dict.json') as fid01:
+                param_dict = json.load(fid01)
 
-        super().__init__(data_brick=data_brick, param_dict=param_dict)
+            super().__init__(data_brick=data_brick, param_dict=param_dict)
+        else:
+            super().__init__(**data)
 
         # create brick
         self._parse_brick()
---
+++ src/scripts/outbreak_calib.py
@@ -0,0 +1,57 @@
+"""
+Plot example example
+"""
+
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import corner
+import pyfcf
+
+import paths
+import methods
+
+# setup matplotlib settings
+pyfcf.setup_matplotlib(font_size=12)
+
+# calibration directory
+tpath = paths.emod / "model_calibration01" / "experiment_workshop_calib04"
+
+# load results
+emod_results = methods.CalibBrick(tpath=tpath)
+
+# which vars were included in the calibration? 
+# calib_vars = ['R0_variance', 'indiv_variance_acq', 'correlation_acq_trans']
+calib_vars = list(emod_results.param_calib["EXP_OPTIMIZE"].keys())
+ndim = len(calib_vars)
+
+# select what to plot
+df_emod_results = emod_results.to_df()
+select_emod_results = df_emod_results[df_emod_results["iteration"] >= 0 ]
+samples = select_emod_results[calib_vars].to_numpy()
+
+numpy_range = lambda x: (x.max() - x.min())
+cnrm = lambda x: (x - df_emod_results['cal_val'].min())/numpy_range(df_emod_results['cal_val'])
+
+
+# corner figure
+label_dict = dict(
+    R0 = 'Reproductive\nNumber',
+    R0_variance = 'Spreader\nHeterogeneity',
+    indiv_variance_acq = 'Catcher\nHeterogeneity',
+    correlation_acq_trans = 'Spreader-Catcher\nCorrelation'
+)
+fig = corner.corner(samples, labels=[label_dict[k] for k in calib_vars])
+
+# Extract the axes
+axes = np.array(fig.axes).reshape((ndim, ndim))
+
+
+# Loop over the histograms
+for yi in range(ndim):
+    for xi in range(yi):
+        ax = axes[yi, xi]
+        ax.scatter(select_emod_results[calib_vars[xi]], select_emod_results[calib_vars[yi]], 
+            s = 3, c=cnrm(select_emod_results['cal_val']))
+
+plt.savefig(paths.figures / "tmp.png")
