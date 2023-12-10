c6518c1198a504dd8fc766cd133b3b7c45f76adc
Katherine Rosenfeld <krosenfeld@idmod.org>
2023-12-10 18:59:13 +0000

    starting calibration

---
+++ emod/model_calibration01/Assets/python/dtk_post_process.py
@@ -0,0 +1,53 @@
+#********************************************************************************
+#
+#********************************************************************************
+
+import os, sys, json
+
+import global_data as gdata
+
+import numpy as np
+
+#********************************************************************************
+
+def application(output_path):
+
+  SIM_IDX       = gdata.sim_index
+
+
+  # Prep output dictionary
+  key_str    = '{:05d}'.format(SIM_IDX)
+  parsed_dat = {key_str: dict()}
+
+
+  # Calculate total attack rate and store in a json dict
+  with open(os.path.join(output_path,'InsetChart.json')) as fid01:
+    inset_chart = json.load(fid01)
+
+  new_inf = np.array(inset_chart['Channels']['New Infections']['Data'])
+  pop_vec = np.array(inset_chart['Channels']['Statistical Population']['Data'])
+
+  tot_pop = pop_vec[-1]
+  max_inf = np.argmax(new_inf)
+  tot_inf = np.sum(new_inf)
+  epi_inf = np.sum(new_inf[:max_inf])
+
+  parsed_dat[key_str]['atk_frac']  = int(tot_inf)/tot_pop
+  parsed_dat[key_str]['herd_frac'] = int(epi_inf)/tot_pop
+
+  # assessment metric for calibration (bigger is better --> 0
+  # https://github.com/InstituteforDiseaseModeling/EMOD-Generic-Scripts/blob/main/model_demographics01/Assets/python/dtk_post_process.py
+  parsed_dat[key_str]['cal_val'] = float(-np.abs(int(tot_inf)/tot_pop - 0.4))
+
+  if gdata.var_params.get('return_timeseries', False):
+    # Monthly timeseries
+    parsed_dat[key_str]['timeseries']   = new_inf.tolist()
+
+  # Write output dictionary
+  with open('parsed_out.json','w') as fid01:
+    json.dump(parsed_dat, fid01)
+
+
+  return None
+
+#*******************************************************************************
---
+++ emod/model_calibration01/experiment_workshop_calib01/make01_param_dict.py
@@ -0,0 +1,71 @@
+#********************************************************************************
+#
+#********************************************************************************
+
+import json
+
+import numpy as np
+
+#*******************************************************************************
+
+# This script makes a json dictionary that is used by the pre-processing script
+# in EMOD. Variable names defined here will be available to use in creating
+# the input files. Please don't change the variable name for 'EXP_NAME' or
+# for 'NUM_SIMS' because those are also used in scripts outside of EMOD.
+
+# The pre-process script will open the json dict created by this method. For
+# everything in the 'EXP_VARIABLE' key, that script will assume a list and
+# get a value from that list based on the sim index. For everything in the 
+# 'EXP_CONSTANT' key, it will assume a single value and copy that value.
+
+
+
+# ***** Setup *****
+param_dict = dict()
+
+param_dict['EXP_NAME']     = 'Demog-Workshop-Calib-00'
+param_dict['NUM_SIMS']     =   720
+param_dict['NUM_ITER']     =     6
+param_dict['EXP_VARIABLE'] = dict()
+param_dict['EXP_CONSTANT'] = dict()
+param_dict['EXP_OPTIMIZE'] = dict()
+
+
+# Random number consistency
+np.random.seed(4)
+
+# Convenience naming
+NSIMS = param_dict['NUM_SIMS']
+
+
+# ***** Specify sim-variable parameters *****
+
+param_dict['EXP_VARIABLE']['run_number']          =  list(range(NSIMS))
+
+
+# ***** Parameters to auto-adjust *****
+
+# Log of age-independent multiplier for mortality rates
+
+param_dict['EXP_OPTIMIZE']['R0_variance'] = [0, 1.5]
+param_dict['EXP_OPTIMIZE']['indiv_variance_acq'] = [0, 1.5]
+param_dict['EXP_OPTIMIZE']['correlation_acq_trans'] = [0, 1]
+
+
+# ***** Constants for this experiment *****
+
+# Number of days for simulation
+param_dict['EXP_CONSTANT']['num_tsteps']   =  1000.0
+
+# R0 value for tranmssion
+param_dict['EXP_CONSTANT']['R0']           =  1.3
+
+
+# ***** Write parameter dictionary *****
+
+with open('param_dict.json','w') as fid01:
+  json.dump(param_dict,fid01)
+
+
+
+#*******************************************************************************
